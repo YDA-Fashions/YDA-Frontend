5d656667d93e7fb1a544c3526c3497b46a333fa5
mac <mac@macs-MacBook-Pro.local>
2026-05-27 21:35:35 +0530

    Update GTM integration using Next.js Head and Script components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Prata } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -81,7 +83,17 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${prata.variable} h-full antialiased font-sans`}
     >
+      <Head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MWVBGWZQ');`
+          }}
+        />
+      </Head>
       <body className="min-h-full flex flex-col">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MWVBGWZQ" height="0" width="0" style={{display: "none", visibility: "hidden"}}></iframe></noscript>
         <JsonLd
           data={{
             "@context": "https://schema.org",
